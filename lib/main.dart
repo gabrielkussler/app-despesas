727c0e8d97e5384a8809005f61bfdec26b2215bd
gabriel <gkgabrielkussler@gmail.com>
2021-07-05 13:31:24 -0300

    interpolação de Strings R$, com duas casas decimais

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,7 @@ class MyHomePage extends StatelessWidget {
                       )),
                       padding: EdgeInsets.all(10),
                       child: Text(
-                        tr.value.toString(),
+                        'R\$ ${tr.value.toStringAsFixed(2)}',
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 20,
@@ -71,7 +71,22 @@ class MyHomePage extends StatelessWidget {
                       ),
                     ),
                     Column(
-                      children: [Text(tr.title), Text(tr.date.toString())],
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          tr.title,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          tr.date.toString(),
+                          style: TextStyle(
+                            color: Colors.grey[700],
+                          ),
+                        )
+                      ],
                     )
                   ],
                 ),
